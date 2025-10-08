d38f2658d69a16597b40416215b3eebce904f6a5
RegularCoder239 <217169551+RegularCoder239@users.noreply.github.com>
2025-10-08 08:53:57 +0200

    Add code

---
+++ MatrixPorted/AppearEffect.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixPorted
+{
+    class AppearEffect : Effect {
+		(int, int) fixed_offset;
+		int tick_counter = 1;
+		int text_amount = 0;
+		bool enable_tick_counter = false;
+		public AppearEffect((int, int) fixedoffset, int textamount, bool[,] targetmask, bool[,] mask, (char, int, int)[,] content) : base(targetmask, mask, content)
+		{
+			this.fixed_offset = fixedoffset;
+		}
+		public override void UpdateTimer(Object? __, System.Timers.ElapsedEventArgs _)
+		{
+			Random random = new Random();
+			if (!this.enable_tick_counter) {
+				this.enable_tick_counter = true;
+				foreach ((char, int, int) ele in this.terminalContent) {
+					if (ele.Item1 != ' ') {
+						this.enable_tick_counter = false;
+					}
+				}
+			} else {
+				this.tick_counter += 1;
+			}
+
+			if (this.tick_counter % 4 == 0 && this.enable_tick_counter) {
+				if (this.tick_counter / 4 == text_amount) {
+					this.FinishEffect();
+				} else {
+					int xoffset = this.tick_counter / 4 * 8 - 8 + fixed_offset.Item1;
+					for (int x = xoffset; x < xoffset + 8; x++) {
+						for (int y = 0; y < this.terminalMask.GetLength(1); y++) {
+							if (this.terminalTargetMask[x, y]) {
+								this.terminalMask[x, y] = true;
+								this.terminalContent[x, y] = (
+									(char)(random.Next() % ('z' - '!') + '!'),
+									random.Next() % 128,
+									((random.Next() % 2) * 2 - 1) * (random.Next() % 5 + 1)
+								);
+							} else {
+								this.terminalMask[x, y] = false;
+								this.terminalContent[x, y] = (' ', 0, 0);
+							}
+						}
+					}
+				}
+			}
+
+			
+		}
+    }
+}
---
+++ MatrixPorted/Effect.cs
@@ -0,0 +1,37 @@
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace MatrixPorted
+{
+	public abstract class Effect {
+		public bool[,] terminalTargetMask;
+		public bool[,] terminalMask;
+		public (char, int, int)[,] terminalContent;
+		public Timer updateTimer = new Timer(50);
+		public abstract void UpdateTimer(Object? obj, System.Timers.ElapsedEventArgs _);
+		public event FinishedDelegate Finished;
+	
+		public delegate void FinishedDelegate();
+		public Effect(bool[,] targetmask, bool[,] mask, (char, int, int)[,] terminalContent)
+		{
+			this.terminalTargetMask = targetmask;
+			this.terminalMask = mask;
+			updateTimer.Elapsed += UpdateTimer;
+			updateTimer.AutoReset = true;
+			updateTimer.Enabled = true;
+			this.terminalContent = terminalContent;
+		}
+		protected void FinishEffect()
+		{
+			updateTimer.AutoReset = false;
+			updateTimer.Enabled = false;
+			Finished?.Invoke();
+		}
+		protected void StartEffect()
+		{
+			updateTimer.AutoReset = false;
+			updateTimer.Enabled = false;
+		}
+		protected void StopEffect() => FinishEffect();
+	}
+}
